79ca6ee23d48bd9a57734942c2d31c1247d0946a
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2004-08-11 00:21:25 +0000

    First release: Version 0.10.

--- lib/Chemistry/File/SLN.pm
+++ lib/Chemistry/File/SLN.pm
@@ -42,9 +42,72 @@ Chemistry::File::SLN - SLN linear notation parser/writer
 
 This module parses a SLN (Sybyl Line Notation) string. This is a File I/O
 driver for the PerlMol project.  L<http://www.perlmol.org/>. It registers the
-'sln' format with Chemistry::Mol.
+'sln' format with Chemistry::Mol, and recognizes filenames ending in '.sln'.
+
+Optional attributes for atoms, bonds, and molecules are stored as
+$atom->attr("sln/attr"), $bond->attr("sln/attr"), and $mol->attr("sln/attr"), 
+respectively. Boolean attributes are stored with a value of 'TRUE'. That's the
+way boolean attributes are recognized when writing, so that they can be written
+in the shortened form.
+
+    $sln_attr->{backbone} = 1;
+    # would be ouput as "C[backbone=1]"
+
+    $sln_attr->{backbone} = 'TRUE';
+    # would be ouput as "C[backbone]"
+
+Also note that attribute names are normalized to lowercase on reading.
 
 =head1 OPTIONS
+
+The following options are available when reading:
+
+=over
+
+=item kekulize
+
+Assign bond orders for unsatisfied valences or for aromatic bonds. For example,
+benzene read as C[1]H:CH:CH:CH:CH:CH@1 will be converted internally to 
+something like C[1]H=CHCH=CHCH=CH@1. This is needed if another format or 
+module expects a Kekule representation without an aromatic bond type.
+
+=back
+
+The following options are available when writing:
+
+=over
+
+=item mols
+
+If this option points to an array of molecules, these molecules will be
+written, one per line, as in the example in the SYNOPSYS.
+
+=item aromatic
+
+Detect aromaticity before writing. This will ensure that aromatic bond types
+are used instead of alternate single and double bonds. 
+
+=item unique
+
+Canonicalize before writing, and produce a unique strucure. NOTE: this option
+does not guarantee a unique representation for molecules with bracketed
+attributes.
+
+=item name
+
+Include the name of the molecule ($mol->name) in the output string.
+
+=item coord3d, coords
+
+Include the 3D coordinates of every atom in the molecule in the output string.
+C<coord3d> and C<coords> may be used interchangeably.
+
+=item attr
+
+Output the atom, bond, and molecule attributes found in $mol->attr("sln/attr"),
+etc.
+
+=back
 
 =head1 CAVEATS
 
@@ -60,7 +123,14 @@ following:
 
 =item Markush structures
 
+=item 2D Coordinates
+
 =back
+
+The SLN specification is vague on several points, and I don't have a reference
+implementation available, so I had to make several arbitrary decisions. Also,
+this version of this module has not been tested exhaustively, so please report
+any bugs that you find.
 
 If the parser doesn't understand a string, it only says "syntax error", which
 may not be very helpful.
@@ -73,24 +143,38 @@ my $Parser = Chemistry::File::SLN::Parser->new;
 
 sub name_is {
     my ($self, $name) = @_;
-    $name =~ /\.sln/;
+    $name =~ /\.sln$/i;
+}
+
+sub file_is {
+    $_[0]->name_is($_[1]);
 }
 
 sub parse_string {
     my ($self, $string, %opts) = @_;
+
+    my (@lines) = split /(?:\n|\r\n?)/, $string;
+    my @mols;
+    for my $line (@lines) {
+        my $mol = $self->parse_single_line($line, %opts);
+        return $mol unless wantarray;
+        push @mols, $mol;
+    }
+    @mols;
+}
+
+sub parse_single_line {
+    my ($self, $string, %opts) = @_;
+
     my $mol_class = $opts{mol_class} || "Chemistry::Mol";
 
-    # these two are not used yet...
-    my $atom_class = $opts{atom_class} || $mol_class->atom_class;
-    my $bond_class = $opts{bond_class} || $mol_class->bond_class;
-
-    my $mol = $mol_class->new;
 
     # call the actual yapp-generated parser
     my $tree = $Parser->run($string) or return;
     #use Data::Dumper; print Dumper $tree;
+
+    my $mol = $mol_class->new;
     my @nodes = @{$tree->{chain}};
-    #my $node = shift @nodes;
     my %closures;
     my $last_atom;
     my @stack;
@@ -104,14 +188,14 @@ sub parse_string {
             my $next = shift @nodes;
             if ($next->{closure}) {
                 my $atom = $closures{$next->{closure}};
-                compile_bond($mol, $node, $last_atom, $atom);
+                $self->compile_bond($mol, $node, $last_atom, $atom);
             } else {
-                my $atom = compile_atom($mol, $next, \%closures);
-                compile_bond($mol, $node, $last_atom, $atom);
+                my $atom = $self->compile_atom($mol, $next, \%closures);
+                $self->compile_bond($mol, $node, $last_atom, $atom);
                 $last_atom = $atom;
             }
         } else {  # first atom
-            $last_atom = compile_atom($mol, $node, \%closures);
+            $last_atom = $self->compile_atom($mol, $node, \%closures);
         }
     }
     if ($opts{kekulize}) {
@@ -125,22 +209,25 @@ sub parse_string {
         } elsif ($attr eq 'type') {
             $mol->type($value);
         } elsif ($attr eq 'coord3d') {
-            read_coords($mol, $value);
+            $self->read_coords($mol, $value);
         } else {
             push @sln_attr, $attr, $value;
         }
     }
     $mol->attr("sln/attr", {@sln_attr}) if @sln_attr;
     $mol;
+
 }
 
 sub compile_atom {
-    my ($mol, $node, $closures) = @_;
+    my ($self, $mol, $node, $closures) = @_;
     my $atom = $mol->new_atom(
         symbol          => $node->{symbol},
         hydrogens       => $node->{hcount},
         formal_charge   => $node->{attr}{charge},
     );
+    $atom->attr("sln/attr", $node->{attr});
+    delete $node->{attr}{charge};
     $closures->{$node->{id}} = $atom if $node->{id};
     $atom;
 }
@@ -154,7 +241,7 @@ my %TYPE_TO_ORDER = (
 );
 
 sub compile_bond {
-    my ($mol, $node, $atom1, $atom2) = @_;
+    my ($self, $mol, $node, $atom1, $atom2) = @_;
     my $order = $TYPE_TO_ORDER{$node->{type}};
     if ($order) {
         my $bond = $mol->new_bond(
@@ -162,6 +249,7 @@ sub compile_bond {
             atoms=>[$atom1, $atom2],
             order => $order,
         );
+        $bond->attr("sln/attr", $node->{attr});
         if ($node->{type} eq ':') { 
             $_->aromatic(1) for ($atom1, $atom2, $bond);
         }
@@ -169,7 +257,7 @@ sub compile_bond {
 }
 
 sub read_coords {
-    my ($mol, $coords_str) = @_;
+    my ($self, $mol, $coords_str) = @_;
     $coords_str =~ s/[()]//g;
     my (@coords) = split /,/, $coords_str;
     my $fh = $mol->formula_hash;
@@ -207,76 +295,99 @@ sub write_string {
 
     my $sln;
     for my $mol (@mols) {
-        my $oldmol = $mol;
-        $mol = $mol->clone; 
-        collapse_hydrogens($mol);
-        my @atoms = $mol->atoms; 
-
-        my @id_log;
-        if (@atoms) {
-            if ($opts{unique}) {
-                unless ($atoms[0]->attr("canon/class")) {
-                    require Chemistry::Canonicalize;
-                    Chemistry::Canonicalize::canonicalize($mol);
-                }
-                $opts{aromatic} = 1; # all unique sln have to be aromatic
-                @atoms = sort {
-                    $a->attr("canon/class") <=> $b->attr("canon/class")
-                } @atoms;
-            }
-
-            if ($opts{aromatic}) {
-                require Chemistry::Ring;
-                Chemistry::Ring::aromatize_mol($mol);
-            }
-
-            my $visited = {};
-            my @s;
-            for my $atom (@atoms) {
-                next if $visited->{$atom};
-                my $ring_atoms = {};
-
-                # first pass to find and number the ring bonds
-                find_ring_bonds($mol, \%opts, $atom, undef, {}, $ring_atoms);
-
-                # second pass to actually generate the sln string
-                push @s, branch($mol, \%opts, $atom, undef, $visited, 
-                    $ring_atoms, \@id_log);
-            }
-            $sln .= join '.', @s;
-        }
-
-        if ($opts{name} or $opts{attr} or $opts{coords} or $opts{coord3d}) {
-            my @attr;
-            push @attr, 'name="' . $mol->name . '"' 
-                if $opts{name} and length $mol->name;
-            my @coords;
-            if ($opts{coord3d} or $opts{coords}) {
-                my @all_atoms = map { 
-                    (
-                        $oldmol->by_id($_), 
-                        grep {$_->symbol eq 'H'}
-                            $oldmol->by_id($_)->neighbors
-                    )
-                } @id_log;
-                push @coords, sprintf("(%.3f,%.3f,%.3f)",$_->coords->array)
-                    for @all_atoms;
-                push @attr, 'coord3d=' . join(',',@coords);
-            }
-            if ($opts{attr}) {
-                while (my ($key, $val) = each %{$mol->attr("sln/attr")||{}}) {
-                    push @attr, "$key" . ($val eq 'TRUE' ? "" : "=$val");
-                }
-            }
-            $sln .= '<' . join(';', @attr) . '>' if @attr;
-        }
-        $sln .= $eol;
+        $sln .= $self->write_mol($mol, %opts) . $eol;
     }
     $sln;
 }
 
+sub write_mol {
+    my ($self, $mol, %opts) = @_;
+
+    my $oldmol = $mol;
+    $mol = $mol->clone; 
+
+    my $sln = '';
+    my @id_log;
+    if ($mol->atoms) {
+        my @atoms = $self->clean_mol($mol, %opts);
+
+        my $visited = {};
+        my @s;
+        for my $atom (@atoms) {
+            next if $visited->{$atom};
+            my $ring_atoms = {};
+
+            # first pass to find and number the ring bonds
+            $self->find_ring_bonds($mol, \%opts, $atom, undef, {}, $ring_atoms);
+
+            # second pass to actually generate the sln string
+            push @s, $self->branch($mol, \%opts, $atom, undef, $visited, 
+                $ring_atoms, \@id_log);
+        }
+        $sln .= join '.', @s;
+    }
+
+    $sln .= $self->format_ctab_attr($mol, \%opts, $oldmol, \@id_log);
+}
+
+sub clean_mol {
+    my ($self, $mol, %opts) = @_;
+
+    $self->collapse_hydrogens($mol);
+    my @atoms = $mol->atoms;
+    if ($opts{unique}) {
+        unless ($atoms[0]->attr("canon/class")) {
+            require Chemistry::Canonicalize;
+            Chemistry::Canonicalize::canonicalize($mol);
+        }
+        #$opts{aromatic} = 1; # all unique sln have to be aromatic
+        @atoms = sort {
+            $a->attr("canon/class") <=> $b->attr("canon/class")
+        } @atoms;
+    }
+
+    if ($opts{aromatic}) {
+        require Chemistry::Ring;
+        Chemistry::Ring::aromatize_mol($mol);
+    }
+    @atoms;
+}
+
+sub format_ctab_attr {
+    my ($self, $mol, $opts, $oldmol, $id_log) = @_;
+
+    my $sln = '';
+    if ($opts->{name} or $opts->{attr} or $opts->{coords} or $opts->{coord3d}) {
+        no warnings 'uninitialized';
+        my @attr;
+        my $name = $mol->name;
+        $name =~ s/[\r\n]//g;
+        push @attr, 'name="' . $mol->name . '"' 
+            if $opts->{name} and length $mol->name;
+        my @coords;
+        if ($opts->{coord3d} or $opts->{coords}) {
+            my @all_atoms = map { 
+                (
+                    $oldmol->by_id($_), 
+                    grep {$_->symbol eq 'H'}
+                        $oldmol->by_id($_)->neighbors
+                )
+            } @$id_log;
+            push @coords, sprintf("(%.3f,%.3f,%.3f)",$_->coords->array)
+                for @all_atoms;
+            push @attr, 'coord3d=' . join(',',@coords);
+        }
+        if ($opts->{attr}) {
+            push @attr, $self->format_sln_attr($mol);
+        }
+        $sln .= '<' . join(';', @attr) . '>' if @attr;
+    }
+    $sln;
+}
+
+
 sub find_ring_bonds {
-    my ($mol, $opts, $atom, $from_bond, $visited, $ring_atoms) = @_;
+    my ($self, $mol, $opts, $atom, $from_bond, $visited, $ring_atoms) = @_;
 
     $visited->{$atom}  = 1;
     for my $bn (sorted_bonds_neighbors($atom, $opts)) {
@@ -288,23 +399,24 @@ sub find_ring_bonds {
             #print "closing ring\n";
             $ring_atoms->{$nei}++;
         } else {
-            find_ring_bonds($mol, $opts, $nei, $bond, $visited, $ring_atoms);
+            $self->find_ring_bonds($mol, $opts, $nei, 
+                $bond, $visited, $ring_atoms);
         }
     }
 }
 
 sub branch {
-    my ($mol, $opts, $atom, $from_bond, $visited, $digits, $id_log) = @_;
+    my ($self, $mol, $opts, $atom, $from_bond, $visited, $digits, $id_log) = @_;
 
     my $prev_branch = "";
     my $sln;
-    $sln .= bond_symbol($from_bond, $opts);
+    $sln .= $self->format_bond($from_bond, $opts);
     my $digit;
     if ($digits->{$atom}) {  # opening a ring
-        $digit = next_digit($digits);
+        $digit = $self->next_digit($digits);
         $digits->{$atom} = $digit;
     }
-    $sln .= format_atom($atom, $opts, $digit);
+    $sln .= $self->format_atom($atom, $opts, $digit);
     push @$id_log, $atom->id;
 
     $visited->{$atom}  = 1;
@@ -319,10 +431,11 @@ sub branch {
             if ($prev_branch) {
                 $sln .= "($prev_branch)";
             }
-            $prev_branch = bond_symbol($bond, $opts) . '@' . $digits->{$nei};
+            $prev_branch = $self->format_bond($bond, $opts) 
+                . '@' . $digits->{$nei};
             $visited->{$bond} = 1;
         } else {
-            my $branch = branch($mol, $opts, $nei, $bond, $visited, 
+            my $branch = $self->branch($mol, $opts, $nei, $bond, $visited, 
                 $digits, $id_log);
             if ($prev_branch) {
                 $sln .= "($prev_branch)";
@@ -335,12 +448,12 @@ sub branch {
 }
 
 sub next_digit {
-    my ($digits) = @_;
+    my ($self, $digits) = @_;
     ++$digits->{used_digits};
 }
 
 sub collapse_hydrogens {
-    my ($mol) = @_;
+    my ($self, $mol) = @_;
 
     for my $atom (grep {$_->symbol eq 'H'} $mol->atoms) {
         my ($neighbor) = $atom->neighbors or next;
@@ -363,18 +476,22 @@ sub sorted_bonds_neighbors {
 }
 
 my %ORDER_TO_TYPE = (
-    2 => '=', 1 => '', 3 => '#',
+    1 => '', 2 => '=', 3 => '#', 4 => '', 0 => '.', 
 );
 
-sub bond_symbol {
-    my ($bond, $opts) = @_;
+sub format_bond {
+    my ($self, $bond, $opts) = @_;
     return '' unless $bond;
-    return ':' if $bond->aromatic;
-    return $ORDER_TO_TYPE{$bond->order};
+    my $s = $bond->aromatic ? ':' : $ORDER_TO_TYPE{$bond->order};
+    my @attr = $self->format_sln_attr($bond) if $opts->{attr};
+    if (@attr) {
+        $s .= '[' . join(";", @attr) . ']';
+    }
+    $s;
 }
 
 sub format_atom {
-    my ($atom, $opts, $digit) = @_;
+    my ($self, $atom, $opts, $digit) = @_;
     my $s;
     no warnings 'uninitialized';
     my $h_count = $atom->hydrogens;
@@ -385,17 +502,31 @@ sub format_atom {
     $h_count = $h_count ? ($h_count > 1 ? "H$h_count" : 'H') : '';
 
     $s = $symbol;
-    if ($charge or $digit) {
+    my @attr = $self->format_sln_attr($atom) if $opts->{attr};
+    if ($charge or $digit or @attr) {
         $s .= '['; 
         $s .= $digit;
-        $s .= ':' if $charge and $digit;
-        $s .= $charge;
-        $s .= ']';
+        unshift @attr, $charge if $charge;
+        if (@attr) {
+            $s .= ':' if $digit;
+            $s .= join ';', @attr;
+       }
+       $s .= ']';
     }
     $s .= $h_count;
     $s;
 }
 
+sub format_sln_attr {
+    my ($self, $obj) = @_;
+    my $sln_attr = $obj->attr("sln/attr") || {};
+    my @attr;
+    for my $key (sort keys %$sln_attr) {
+        my $val = $sln_attr->{$key};
+        push @attr, "$key" . ($val eq 'TRUE' ? "" : "=$val");
+    }
+    @attr;
+}
 
 1;
 
@@ -411,16 +542,17 @@ The PerlMol website L<http://www.perlmol.org/>
 
 Ash, S.; Cline, M. A.; Homer, R. W.; Hurst, T.; Smith, G. B., SYBYL Line
 Notation (SLN): A Versatile Language for Chemical Structure Representation. J.
-Chem. Inf. Comput. Sci; 1997; 37(1); 71-79.  DOI: 10.1021/ci960109j
+Chem. Inf. Comput. Sci; 1997; 37(1); 71-79.  DOI: 10.1021/ci960109j 
+(L<http://dx.doi.org/10.1021/ci960109j>)
 
 =head1 AUTHOR
 
-Ivan Tubert E<lt>itub@cpan.orgE<gt>
+Ivan Tubert-Brohman E<lt>itub@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004 Ivan Tubert. All rights reserved. This program is free
-software; you can redistribute it and/or modify it under the same terms as
+Copyright (c) 2004 Ivan Tubert-Brohman. All rights reserved. This program is
+free software; you can redistribute it and/or modify it under the same terms as
 Perl itself.
 
 =cut
